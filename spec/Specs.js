128c84952378075e5f3190360390a72b769da2c7
Paul Ebreo <pebre79@hotmail.com>
2016-07-11 10:36:13 -0500

    add jasmine tests

---
+++ spec/Specs.js
@@ -0,0 +1,41 @@
+describe("Basic", function() {
+    var logic;
+    beforeEach(function() {
+        //logic = new Funcs();
+
+    });
+
+    it("should know its turn", function() {
+        expect(3).toBe(3);
+
+
+    });
+
+    xit("should know a draw", function() {
+        expect(false).toBe(true);
+
+    });
+
+
+
+});
+
+
+describe("Handlestr", function() {
+    var logic;
+    beforeEach(function() {
+
+    });
+
+    it("should handle computerlost", function() {
+        expect(false).toEqual(false);
+    });
+
+    xit("should handle computerwon", function() {
+        expect(9).toEqual(3);
+
+    });
+
+
+
+});
