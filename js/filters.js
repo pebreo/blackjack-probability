82fd3acda81071887b76ec40eacee48c932a5a2c
Paul Ebreo <pebre79@hotmail.com>
2016-07-12 15:37:25 -0500

    add filter to display card better

---
+++ js/filters.js
@@ -0,0 +1,39 @@
+(function () {
+    //'use strict';
+
+    var app = angular.module('myApp');
+
+    app.filter('unsafe', function(){
+        return function(input) {
+
+            //$scope.h = "&" + rank + ";";
+            //return rank;
+            return rank.trustAsHtml;
+        }
+    });
+
+    app.filter('capface', function(){
+        return function(input){
+            var rank = '';
+            switch(input) {
+                case 'a':
+                    rank = 'A';
+                    break;
+                case 'j':
+                    rank = 'J';
+                    break;
+                case 'q':
+                    rank = 'Q';
+                    break;
+                case 'k':
+                    rank = 'K';
+                    break;
+                default:
+                    rank = input;
+            }
+            return rank;
+        }
+    });
+
+
+}());
