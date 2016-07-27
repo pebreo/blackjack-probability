f00ffcf08b8b1ef01ca6db8c3ffdfeebc93bb0c1
Paul Ebreo <pebre79@hotmail.com>
2016-07-27 10:20:57 -0500

    redefine test description of test purpose

--- spec/Spec-stats.js
+++ spec/Spec-stats.js
@@ -14,17 +14,36 @@ describe("get_prob_stats", function() {
         stats = _stats_;
     }));
 
-
-
-    xit("should run Ace + Ace", function() {
+    it("should run King + 8", function() {
         /*
             3 - all suits
             2 (all suits) + A (all suits)
             A (clubs,diams,hearts) + A(diams, hearts, spades)
             A (all suits)
         */
-        var player_hand =  stub_data.make_hand([ ['a','spades'], ['a','hearts'] ]);
+        var player_hand =  stub_data.make_hand([ ['k','spades'], ['8','hearts'] ]);
         var dealer_hand =  stub_data.make_hand([ ['8','hearts'], ['3','hearts'] ]);
+        dealer_hand[1].show = false;
+         var static_deck = stub_data.static_deck;
+        hand_value = myservice.calc_hand_value(player_hand);
+        var desired_hand_value = 21 - hand_value[0];
+        var avail_deck = transform.get_available_cards(static_deck, player_hand, dealer_hand);
+         console.log(avail_deck.length);
+        var result = stats.get_prob_stats(player_hand, avail_deck);
+        console.log(result);
+        expect(result.combos_count.length).toEqual(3);
+
+    });
+
+    xit("should not crash when run Ace + Ace", function() {
+        /*
+            3 - all suits
+            2 (all suits) + A (all suits)
+            A (clubs,diams,hearts) + A(diams, hearts, spades)
+            A (all suits)
+        */
+        var player_hand =  stub_data.make_hand([ ['a','diams'], ['a','spades'] ]);
+        var dealer_hand =  stub_data.make_hand([ ['i','hearts'], ['3','hearts'] ]);
         dealer_hand[1].show = false;
          var static_deck = stub_data.static_deck;
         hand_value = myservice.calc_hand_value(player_hand);
@@ -56,25 +75,6 @@ describe("get_prob_stats", function() {
 
     });
 
-    xit("should run King + 8", function() {
-        /*
-            3 - all suits
-            2 (all suits) + A (all suits)
-            A (clubs,diams,hearts) + A(diams, hearts, spades)
-            A (all suits)
-        */
-        var player_hand =  stub_data.make_hand([ ['k','spades'], ['8','hearts'] ]);
-        var dealer_hand =  stub_data.make_hand([ ['8','hearts'], ['3','hearts'] ]);
-        dealer_hand[1].show = false;
-         var static_deck = stub_data.static_deck;
-        hand_value = myservice.calc_hand_value(player_hand);
-        var desired_hand_value = 21 - hand_value[0];
-        var avail_deck = transform.get_available_cards(static_deck, player_hand, dealer_hand);
-         console.log(avail_deck.length);
-        var x = stats.get_prob_stats(player_hand, avail_deck);
-        expect(x.combos_count.length).toEqual(3);
-
-    });
 
     xit("should run 2 + 2", function() {
         /*
