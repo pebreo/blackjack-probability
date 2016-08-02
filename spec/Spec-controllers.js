3eb1d84610807b56e0be36eb8f3d58472b68dc09
Paul Ebreo <pebre79@hotmail.com>
2016-08-02 08:55:51 -0500

    improve Spec-controllers to nest tests

--- spec/Spec-controllers.js
+++ spec/Spec-controllers.js
@@ -5,21 +5,35 @@ i have to clean dh_by_count, line 691
  */
 
 
-describe("get_prob_stats", function() {
+describe("MyCtrl", function() {
     var $controller;
+
     // setup the angular module
     beforeEach(module('myApp'));
 
-    // setup the service
+    // setup the controller
     beforeEach(inject(function(_$controller_) {
        $controller = _$controller_;
     }));
 
-    it("should have a controller", function() {
-        var $scope = {};
-        var controller = $controller('MyCtrl', { $scope: $scope });
+    // nested tests
+    describe("$scope.is_end()", function(){
+        var $scope, controller;
+
+        beforeEach(function() {
+            $scope = {};
+            controller = $controller('MyCtrl', { $scope: $scope });
+        });
+
+        it("should have is_end()", function() {
+           console.log($scope.is_end());
+
+        });
+
 
     });
+
+
 
 
 });
