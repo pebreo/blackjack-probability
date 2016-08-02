df88b033a143856943bce1805689c7f56e8fac41
Paul Ebreo <pebre79@hotmail.com>
2016-08-02 08:46:53 -0500

    add controller test skeleton

---
+++ spec/Spec-controllers.js
@@ -0,0 +1,27 @@
+/*
+
+i have to clean dh_by_count, line 691
+
+ */
+
+
+describe("get_prob_stats", function() {
+    var $controller;
+    // setup the angular module
+    beforeEach(module('myApp'));
+
+    // setup the service
+    beforeEach(inject(function(_$controller_) {
+       $controller = _$controller_;
+    }));
+
+    it("should have a controller", function() {
+        var $scope = {};
+        var controller = $controller('MyCtrl', { $scope: $scope });
+
+    });
+
+
+});
+
+
